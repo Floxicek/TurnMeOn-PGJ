a13e01dc4dc82b1fcd337bfe9371f679f67181b9
Jan <jan@ejhle.net>
2024-11-30 23:07:28 +0100

    prototype of text displayer

--- scripts/text_display.gd
+++ scripts/text_display.gd
@@ -2,127 +2,93 @@ extends Node2D
 
 var rng = RandomNumberGenerator.new()
 
-var shown_text = ""
-@export var dialogue = ["Sample text"]
+@export var dialogue = []
 @export var time_between_letters = 0.2
-@export var time_between_dialogues = 1
+#@export var time_between_dialogues = 1
 @export var wait_before_hide:float = 3
-@export var hide_afterwards:bool = false
-@export var loop_dialogue:bool = false
+#@export var hide_afterwards:bool = false
+#@export var loop_dialogue:bool = false
 
-# TODO fix
 
-var text_complete:bool = false
+@onready var label = $Label
+var target_text:String = ""
+var _target_text_index:int = 0
 
-var cur_text:String = "Sample text"
-
-var single_word:bool = false
-
-var cur_text_index:int = 0
 signal finished
 
-var wrong_button_dialogue:Array[String] = ["Nope, not this one.",
-"Nope, nope, nopity, nope.",
-"No.",
-"This button isn’t giving.",
-"Not slay. ",
-"Nuh-uh.",
-"Nice try but no.",
-"That’s the wrong button.",
-"That’s the wrong button again.",
-"Not this one! Look again!"]
-#"This be not the proper button, oh thou mighty knight.",
-#"This be not the path to progress, thou mighty knight.",
-#"Attempt a different path, oh thou sorcerer of the machine."]
-# Called when the node enters the scene tree for the first time.
+@onready var next_letter_timer = $NextLetterTimer
+@onready var next_word_timer = $NextWordTimer
+@onready var hide_text_timer = $HideTextTimer
+
+
+var _wrong_button_dialogue:Array[String] = [
+	"Nope, not this one.",
+	"Nope, nope, nopity, nope.",
+	"No.",
+	"This button isn’t giving.",
+	"Not slay. ",
+	"Nuh-uh.",
+	"Nice try but no.",
+	"That’s the wrong button.",
+	"That’s the wrong button again.",
+	"Not this one! Look again!",
+	#"This be not the proper button, oh thou mighty knight.",
+	#"This be not the path to progress, thou mighty knight.",
+	#"Attempt a different path, oh thou sorcerer of the machine.",
+]
 func _ready() -> void:
-	$Next_letter_timer.wait_time = time_between_letters
-	$Next_word_timer.wait_time = time_between_dialogues
-	$Hide_word_timer.wait_time = wait_before_hide
-	shown_text = ""
+	$NextLetterTimer.wait_time = time_between_letters
+	#$NextWordTimer.wait_time = time_between_dialogues
+	$HideTextTimer.wait_time = wait_before_hide
+	label.text = ""
+
+func _reset_label():
+	label.text = ""
+	_target_text_index = 0
+	hide_text_timer.stop()
+
+func write_dialogue(_dialogue: Array):
+	dialogue = _dialogue
+	if dialogue.size() > 0:
+		_dialogue_next()
+
+func write(text: String):
+	target_text = text
+	$NextLetterTimer.start()
+
+func _dialogue_next():
+	write(dialogue[0])
+	dialogue.pop_front()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	$Label.text = shown_text
-	
-	
-func show_rand_bad_button_dialog():
-	start_printing_single_word(wrong_button_dialogue[rng.randi_range(0,len(wrong_button_dialogue)-1)])
-	
-	
-func start_printing_single_word(text:String, custom_time_to_print:float = -1):
-	$Next_word_timer.stop()
-	$Hide_word_timer.stop()
-	hide_text()
-	single_word = true
-	cur_text = text
-	if custom_time_to_print != -1:
-		$Next_letter_timer.wait_time = custom_time_to_print/len(cur_text)
-	$Next_letter_timer.start()
-	
+func write_wrong_button():
+	write(_wrong_button_dialogue[rng.randi_range(0,len(_wrong_button_dialogue)-1)])
 
-func start_printing(custom_time_to_print:float = -1):
-	single_word = false
-	$Next_word_timer.stop()
-	$Hide_word_timer.stop()
-	cur_text = dialogue[cur_text_index]
-	if custom_time_to_print != -1:
-		$Next_letter_timer.wait_time = custom_time_to_print/len(cur_text)
-	$Next_letter_timer.start()
+func _next_letter():
+	if _target_text_index == 0:
+		hide_text_timer.stop()
+		hide_text_timer.start()
 	
-func finished_word():
-	$Next_letter_timer.stop()
-	
-	if(single_word):
-		single_word=false
-		await get_tree().create_timer(wait_before_hide).timeout
-		finished.emit()
-		if(hide_afterwards):
-			hide_text()
+	elif (_target_text_index) >= len(target_text):
+		$HideTextTimer.start()
+		$NextLetterTimer.stop()
+		target_text = ""
+		_target_text_index = 0
 		return
+	var cur_text = label.text
+	var new_text = label.text + target_text[_target_text_index]
 	
-	#Finished dialogue
-	if(cur_text_index == len(dialogue)-1):
-		if loop_dialogue:
-			cur_text_index=-1
-			$Next_word_timer.start()
-			return
-		
-		if(hide_afterwards):
-			$Hide_word_timer.start()
-			await $Hide_word_timer.timeout
-			finished.emit()
-			hide_text()
-			return
-	
-	$Next_word_timer.start()
-	
-func show_whole_text():
-	finished_word()
-	shown_text = dialogue[cur_text_index]
-	
-func hide_text():
-	$Next_letter_timer.stop()
-	shown_text = ""
-	cur_text_index = 0
-	
-	
-func _on_next_letter_timer_timeout() -> void:
-	var len_text = len(cur_text)
-	for i in range(len(cur_text)+1):
-		if i >= len(shown_text) and i<len(cur_text):
-			shown_text+=cur_text[i]
-			return
-	
-	finished_word()
-	return
+	if not target_text.begins_with(new_text):
+		_reset_label()
+	else:
+		label.text = new_text
+		_target_text_index += 1
 
-func _on_next_word_timer_timeout() -> void:
-	$Next_word_timer.stop()
-	
-	if(cur_text_index < len(dialogue)-1):
-		shown_text = ""
-		cur_text_index +=1
-		cur_text = dialogue[cur_text_index]
-		start_printing()
+
+func _on_hide_text_timer_timeout() -> void:
+#	Check if we have more dialogs
+	_reset_label()
+	if dialogue.size() > 0:
+		_dialogue_next()
+	else:
+		finished.emit()

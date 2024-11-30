23889e60ed3d58d1bcccb754698dd5442d25e132
Jan <jan@ejhle.net>
2024-11-30 12:47:04 +0100

    Create fireboy and watergirl (in progress)

--- scripts/button.gd
+++ scripts/button.gd
@@ -4,22 +4,28 @@ signal hover_start
 signal hover_stopped
 signal pressed
 
+@export var react_on_cursor = true
+
 
 func _ready():
 	$AnimatedSprite2D.play()
 
 var default_scale = 1.0
-@export_range(.8,1.4) var hover_scale = 1.2
+@export_range(.8,1.4) var hover_scale = 1.1
 
 func set_default_scale(_scale):
 	default_scale = _scale
 
 func _on_area_entered(area):
 	if area.is_in_group("Cursor"):
-		hover_start.emit(area)
-		var tween = get_tree().create_tween()
-		tween.tween_property(self, "scale", Vector2(hover_scale * default_scale, hover_scale * default_scale), .05)
-		tween.set_trans(Tween.TRANS_CUBIC)
+		if react_on_cursor:
+			hover_start.emit(area)
+			var tween = get_tree().create_tween()
+			tween.tween_property(self, "scale", Vector2(hover_scale * default_scale, hover_scale * default_scale), .05)
+			tween.set_trans(Tween.TRANS_CUBIC)
+		else:
+			_on_area_exited(area)
+	
 
 func _on_area_exited(area) -> void:
 	if area.is_in_group("Cursor"):
---
+++ scripts/fireboy_watergirl/fireboy_character.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+
+@export var fireboy := true
+const SPEED = 300.0
+@export var jump_velocity = -1000.0
+
+func _ready() -> void:
+	if fireboy:
+		add_to_group("Fireboy")
+	else:
+		add_to_group("Watergirl")
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	var jumping = (Input.is_action_just_pressed("UP") and fireboy) or (Input.is_action_just_pressed("arrow_up") and not fireboy)
+	if (jumping) and is_on_floor():
+		velocity.y = jump_velocity
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	
+	var direction := Input.get_axis("LEFT", "RIGHT") if fireboy else Input.get_axis("arrow_left", "arrow_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func die():
+	hide()

496d102e55ff17b52e5f1b8146a20210454146cb
Jan <jan@ejhle.net>
2024-11-30 08:49:15 +0100

    Tween button hover size

--- scripts/button.gd
+++ scripts/button.gd
@@ -12,9 +12,12 @@ func _process(delta):
 
 func _on_area_2d_area_entered(area):
 	if area is Cursor_object:
-		scale = Vector2(1.2, 1.2)
-
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "scale", Vector2(1.2, 1.2), .05)
+		tween.set_trans(Tween.TRANS_CUBIC)
 
 func _on_area_2d_area_exited(area):
 	if area is Cursor_object:
-		scale = Vector2(1, 1)
+		var tween = get_tree().create_tween()
+		tween.tween_property(self, "scale", Vector2(1, 1), .05)
+		tween.set_trans(Tween.TRANS_CUBIC)

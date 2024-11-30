825237696d807809b13d921e8b266d9140074a22
Tondastro <tonik.voracek@gmail.com>
2024-11-30 16:12:57 +0100

    added sound effect player

--- autoload/audio_player.gd
+++ autoload/audio_player.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+enum SFX_Type {CLICK = 0, WRONG_CLICK = 1 }
+
+@export var sound_effects: Array[Array]
+var rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -15,3 +19,9 @@ func pause_soundratck():
 	
 func play_soundtrack():
 	$SoundtrackPlayer.stream_paused = false
+
+func play_sound_effect(type: SFX_Type):
+	var index = rng.randi_range(0, len(sound_effects[type]) - 1)
+	$SoundEffectPlayer.stream = sound_effects[index]
+	$SoundEffectPlayer.play()
+	

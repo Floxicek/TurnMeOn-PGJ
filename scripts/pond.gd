f63ff4458851e5ba63f932e086507f8166ad51b5
Jan <jan@ejhle.net>
2024-11-30 16:20:09 +0100

    Fix pond

--- scripts/pond.gd
+++ scripts/pond.gd
@@ -2,21 +2,18 @@ extends Area2D
 
 enum LiquidType {LAVA, WATER, ACID}
 @export var type: LiquidType = LiquidType.WATER
-@export_color_no_alpha var lava_color
-@export_color_no_alpha var water_color
-@export_color_no_alpha var acid_color
+@export_color_no_alpha var lava_color = Color("c24545")
+@export_color_no_alpha var water_color = Color("4771cc")
+@export_color_no_alpha var acid_color = Color("5f841d")
 
 
 func _ready() -> void:
 	if type == LiquidType.LAVA:
-		$AnimatedSprite2D.play("lava")
-		$AnimatedSprite2D.modulate = lava_color
+		modulate = lava_color
 	elif type == LiquidType.WATER:
-		$AnimatedSprite2D.play("water")
-		$AnimatedSprite2D.modulate = water_color
+		modulate = water_color
 	elif type == LiquidType.ACID:
-		$AnimatedSprite2D.play("acid")
-		$AnimatedSprite2D.modulate = acid_color
+		modulate = acid_color
 
 func _on_body_entered(body: Node2D) -> void:
 	if (type == LiquidType.WATER and body.is_in_group("Fireboy")) or (type == LiquidType.LAVA and body.is_in_group("Watergirl")) or (type == LiquidType.ACID and (body.is_in_group("Fireboy") or body.is_in_group("Watergirl"))):

5d7ae8caa08e23e0df7150b1a0143cf239b81d19
David Říha <davedave0008@gmail.com>
2024-11-30 09:41:07 +0100

    Added functions to text_displayer.tscn

--- scripts/temp_rectangle.gd
+++ scripts/temp_rectangle.gd
@@ -13,8 +13,10 @@ func _process(delta: float) -> void:
 
 func _on_area_entered(area: Area2D) -> void:
 	var sprite:Sprite2D = $rectangle
+	get_parent().get_node("Text_displayer").start_printing(1)
 	sprite.self_modulate = Color.RED
 
 func _on_area_exited(area: Area2D) -> void:
 	var sprite:Sprite2D = $rectangle
+	get_parent().get_node("Text_displayer").hide_text()
 	sprite.self_modulate = Color.BLUE
--- scripts/text_display.gd
+++ scripts/text_display.gd
@@ -6,15 +6,26 @@ var shown_text = ""
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Next_letter_timer.wait_time = time_between_letters
-	$Next_letter_timer.start()
-	pass # Replace with function body.
+	shown_text = ""
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	$Label.text = shown_text
-	pass
 
+
+func start_printing(custom_time_to_print:float = -1):
+	if custom_time_to_print != -1:
+		$Next_letter_timer.wait_time = custom_time_to_print/len(text)
+	$Next_letter_timer.start()
+	
+func show_whole_text():
+	$Next_letter_timer.stop()
+	shown_text = text
+	
+func hide_text():
+	$Next_letter_timer.stop()
+	shown_text = ""
 
 func _on_next_letter_timer_timeout() -> void:
 	var len_text = len(text)

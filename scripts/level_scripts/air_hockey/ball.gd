b1e5e0e8f52ae0c6951149ce10919df599d75894
David Říha <davedave0008@gmail.com>
2024-12-01 11:44:01 +0100

    Finished ice hockey

--- scripts/level_scripts/air_hockey/ball.gd
+++ scripts/level_scripts/air_hockey/ball.gd
@@ -3,33 +3,46 @@ extends Area2D
 class_name Ball
 
 @export var velocity: Vector2 = Vector2.RIGHT*50
+@export var rotation_speed: float = 50
 
 var recently_bounced_x: bool = false
 var recently_bounced_y: bool = false
+
+var rotating_right:= false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 func _physics_process(delta: float) -> void:
-	position += delta*velocity
+	rotation += pow(-1, int(rotating_right)) * rotation_speed*delta
+	position += delta*velocity*100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	pass
 
 func bounce_x():
 	if(recently_bounced_x): return
+	rotating_right = not rotating_right
 	velocity = Vector2(-velocity.x,velocity.y)
 	recently_bounced_x = true
 	$XBounceDeltaTimer.start()
 func bounce_y():
 	if(recently_bounced_y): return
+	rotating_right = not rotating_right
 	velocity = Vector2(velocity.x,-velocity.y)
 	recently_bounced_y = true
 	$YBounceDeltaTimer.start()
 	
 
+func hit_with_paddle(paddle: Hockey_paddle):
+	rotating_right = not rotating_right
+	var velocity_length = velocity.length()
+	var new_velocity = velocity + paddle.velocity
+	velocity = paddle.velocity
+	#velocity = new_velocity.normalized()*velocity_length
+	return
 
 func _on_x_bounce_delta_timer_timeout() -> void:
 	recently_bounced_x = false
--- scripts/level_scripts/air_hockey/wall.gd
+++ scripts/level_scripts/air_hockey/wall.gd
@@ -2,10 +2,11 @@ extends Node2D
 
 @export var horizontal: bool = true
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$AnimatedSprite2D.play("default")
+	var sprite = get_node_or_null("AnimatedSprite2D")
+	if sprite != null:
+		sprite.play("default")
 	pass # Replace with function body.
 
 
@@ -17,17 +18,23 @@ func _process(delta: float) -> void:
 func _on_left_area_entered(area: Area2D) -> void:
 	if (area is Ball):
 		area.bounce_x() if horizontal else area.bounce_y()
+	if area is Hockey_paddle:
+		area.wall_collision(horizontal, position)
 
 func _on_right_area_entered(area: Area2D) -> void:
 	if (area is Ball):
 		area.bounce_x() if horizontal else area.bounce_y()
-
+	if area is Hockey_paddle:
+		area.wall_collision(horizontal, position)
 
 func _on_down_area_entered(area: Area2D) -> void:
 	if (area is Ball):
 		area.bounce_y() if horizontal else area.bounce_x()
-
+	if area is Hockey_paddle:
+		area.wall_collision(not horizontal, position)
 
 func _on_up_area_entered(area: Area2D) -> void:
 	if (area is Ball):
 		area.bounce_y() if horizontal else area.bounce_x()
+	if area is Hockey_paddle:
+		area.wall_collision(not horizontal, position)

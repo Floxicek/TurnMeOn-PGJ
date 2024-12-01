22baec39774dda69767d2a15ad6e8a0d14a76fca
Jan <jan@ejhle.net>
2024-12-01 16:53:57 +0100

    fix scene order

--- autoload/SceneManager.gd
+++ autoload/SceneManager.gd
@@ -15,8 +15,8 @@ var levels: Array = [
 	"res://scenes/levels/level_invert_wasd.tscn",
 	"res://scenes/levels/level_falling.tscn",
 	"res://scenes/levels/level_pong.tscn",
-	"res://scenes/levels/level_periodic.tscn",
 	"res://scenes/levels/level_fireboy_and_watergirl_again.tscn",
+	"res://scenes/levels/level_periodic.tscn",
 	"res://scenes/levels/level_eater.tscn",
 	"res://scenes/levels/air_hockey.tscn",
 	"res://scenes/credits.tscn"

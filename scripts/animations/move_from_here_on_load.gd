ed7096626b59aadbb7dbdbb2a4741c1093abbea9
Jan <jan@ejhle.net>
2024-12-01 15:43:58 +0100

    Fix editro scripts

--- scripts/animations/move_from_here_on_load.gd
+++ scripts/animations/move_from_here_on_load.gd
@@ -1,37 +1,17 @@
-#@tool
 extends Marker2D
-class_name MoveHereOnLoad
+class_name MoveFromHereOnLoad
 
 @export var duration := 1.0
 
-var last_parent_position: Vector2 = Vector2.ZERO
+var child_pos: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
-	if not Engine.is_editor_hint():
+	if get_children().size() > 0:
+		child_pos = get_child(0).global_position
+		get_child(0).global_position = global_position
 		await SceneManager.transition_done
-		var parent = get_parent()
-		if not parent:
+		var child = get_child(0)
+		if not child:
 			return
 		var tween = get_tree().create_tween()
-		tween.tween_property(parent, "global_position", global_position, duration)
-		last_parent_position = parent.global_position
-
-func _process(delta: float) -> void:
-	if Engine.is_editor_hint():
-		var parent = get_parent()
-		if not parent:
-			return
-		if parent.global_position != last_parent_position or global_position != position:
-			last_parent_position = parent.global_position
-			queue_redraw()
-
-func _draw() -> void:
-	if Engine.is_editor_hint():
-		var parent = get_parent()
-		if parent:
-			var start_pos = parent.global_position - global_position
-			var end_pos = Vector2.ZERO
-			draw_line(start_pos, end_pos, Color(1, 0, 0), 2)
-			var arrow_size = 10
-			var direction = (end_pos - start_pos).normalized()
-			var arrow_tip = start_pos + direction * arrow_size
+		tween.tween_property(child, "global_position", child_pos, duration)
--- scripts/animations/move_here_on_load.gd
+++ scripts/animations/move_here_on_load.gd
@@ -1,19 +1,16 @@
-#@tool
 extends Marker2D
-class_name MoveFromHereOnLoad
+class_name MoveHereOnLoad
 
 @export var duration := 1.0
 
-var child_pos: Vector2 = Vector2.ZERO
+var last_parent_position: Vector2 = Vector2.ZERO
 
 func _ready() -> void:
 	if not Engine.is_editor_hint():
-		if get_child(0):
-			child_pos = get_child(0).global_position
-			get_child(0).global_position = global_position
-			await SceneManager.transition_done
-			var child = get_child(0)
-			if not child:
-				return
-			var tween = get_tree().create_tween()
-			tween.tween_property(child, "global_position", child_pos, duration)
+		await SceneManager.transition_done
+		var parent = get_parent()
+		if not parent:
+			return
+		var tween = get_tree().create_tween()
+		tween.tween_property(parent, "global_position", global_position, duration)
+		last_parent_position = parent.global_position
